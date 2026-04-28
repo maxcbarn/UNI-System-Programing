419416cba27f21d4829e89846cb8e57f9a732109
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2026-04-28 15:52:31 -0300

    Add FLAGS enum for system architecture

---
+++ src/SystemArchitecture.hpp
@@ -0,0 +1,8 @@
+enum FLAGS {
+    CARRY           = 0x01, // Bit 0
+    ADD_SUBTRACT    = 0x02, // Bit 1
+    PARITY_OVERFLOW = 0x04, // Bit 2
+    HALF_CARRY      = 0x10, // Bit 4
+    ZERO            = 0x40, // Bit 6
+    SIGN            = 0x80  // Bit 7
+};
