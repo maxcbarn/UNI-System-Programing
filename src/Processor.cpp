b33f16c4322b8c3d353b862851ccfa70b1119748
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2026-04-28 15:26:17 -0300

    Implement instruction execution in NextInstruction
    Ciclo de Execução do seu processador, com O Z80 tem uma instrução chamada HALT (requisito 10.6).

--- src/Processor.cpp
+++ src/Processor.cpp
@@ -31,7 +31,17 @@ void Processor::Initialize( INPUTADRESSINGTYPES adressingTypes ) {
 }
 
 bool Processor::NextInstruction() {
-    return false;
+   if (halt) return false;
+
+    Registers* regs = Registers::GetRegisters();
+    Memory* mem = Memory::GetMemory();
+    
+    Word opcode = mem->ReadMemory(regs->GetPC());
+    
+    this->adressingTypes->Execute(opcode); 
+
+    regs->IncrementPC(); 
+    return true;
 }
 
 void Processor::LoadProgram() {
